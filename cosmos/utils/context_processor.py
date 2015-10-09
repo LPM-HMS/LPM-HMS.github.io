edd147c62e6be8e1b99d448bad63ac0c5e058528
yassineS <yassinesouilmi@gmail.com>
2015-10-09 22:04:47 +0100

    adding readme file

--- cosmos/utils/context_processor.py
+++
@@ -1,9 +0,0 @@
-"""
-Default extra context processor.  Allows cosmos settings to be passed to every Django template.
-"""
-from cosmos.config import settings
-
-def contproc(request):
-    return {
-        'cosmos_settings' : settings
-    }
